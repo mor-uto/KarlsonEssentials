305d825ca094eb76fa63edd29e458b2677fe3209
Moruto <79018613+Mor-uto@users.noreply.github.com>
2024-03-25 05:44:00 +0200

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MoreFps {
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Plugin : BaseUnityPlugin {
+        public const string pluginGuid = "me.moruto.karlson.morefps";
+        public const string pluginName = "MoreFps";
+        public const string pluginVersion = "1.0.0";        
+
+        public void Awake() {
+            Logger.LogInfo("MoreFps by Moruto_ loaded!");
+            SceneManager.sceneLoaded += sceneLoadEvent;
+        }
+
+        private void sceneLoadEvent(Scene scene, LoadSceneMode mode) {
+            foreach (GameObject obj in scene.GetRootGameObjects()) {
+                if (obj.name.ToLower() == "bloom") {
+                    Destroy(obj);
+                }
+
+                if (obj.GetComponentsInChildren<Light>() != null) {
+                    foreach (Light light in obj.GetComponentsInChildren<Light>()) { Destroy(light); }
+                }
+            }
+        }
+    }
+}
