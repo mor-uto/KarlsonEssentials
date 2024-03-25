16e1178f8987f2f791c809a1ff97bace78c61b22
Moruto <79018613+Mor-uto@users.noreply.github.com>
2024-03-25 06:28:41 +0200

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,5 @@
-﻿using BepInEx;
-using System;
-using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,21 +8,29 @@ namespace MoreFps {
     public class Plugin : BaseUnityPlugin {
         public const string pluginGuid = "me.moruto.karlson.morefps";
         public const string pluginName = "MoreFps";
-        public const string pluginVersion = "1.0.0";        
+        public const string pluginVersion = "1.0.0";
+
+        private ConfigEntry<bool> disableBloom;
+        private ConfigEntry<bool> disableLights;
 
         public void Awake() {
             Logger.LogInfo("MoreFps by Moruto_ loaded!");
             SceneManager.sceneLoaded += sceneLoadEvent;
+
+            disableBloom = Config.Bind("General", "disableBloom", true, "Toggle the Bloom (glowing lights) from doorFrames, lamps, weapons, etc...");
+            disableLights = Config.Bind("General", "disableLights", true, "Toggle the lights from any lamp or light source");
         }
 
         private void sceneLoadEvent(Scene scene, LoadSceneMode mode) {
+            if (GameState.Instance.GetGraphics() == true) return;
+
             foreach (GameObject obj in scene.GetRootGameObjects()) {
                 if (obj.name.ToLower() == "bloom") {
-                    Destroy(obj);
+                    if (disableBloom.Value) Destroy(obj);
                 }
 
-                if (obj.GetComponentsInChildren<Light>() != null) {
-                    foreach (Light light in obj.GetComponentsInChildren<Light>()) { Destroy(light); }
+                if (disableLights.Value) {
+                    if (obj.GetComponentsInChildren<Light>() != null) foreach (Light light in obj.GetComponentsInChildren<Light>()) { Destroy(light); }
                 }
             }
         }
