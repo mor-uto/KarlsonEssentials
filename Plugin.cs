2917bfb269bfe90419f78767d441118d90b4d714
Moruto <79018613+Mor-uto@users.noreply.github.com>
2024-05-04 02:38:38 +0300

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -5,33 +5,47 @@ using UnityEngine.SceneManagement;
 
 namespace MoreFps {
     [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-    public class Plugin : BaseUnityPlugin {
+    internal class Plugin : BaseUnityPlugin {
         public const string pluginGuid = "me.moruto.karlson.morefps";
         public const string pluginName = "MoreFps";
-        public const string pluginVersion = "1.0.0";
+        public const string pluginVersion = "1.0.1";
 
         private ConfigEntry<bool> disableBloom;
         private ConfigEntry<bool> disableLights;
 
-        public void Awake() {
-            Logger.LogInfo("MoreFps by Moruto_ loaded!");
-            SceneManager.sceneLoaded += sceneLoadEvent;
+        private ConfigEntry<string> CustomResolution;
+        private ConfigEntry<bool> isFullscreen;
 
-            disableBloom = Config.Bind("General", "disableBloom", true, "Toggle the Bloom (glowing lights) from doorFrames, lamps, weapons, etc...");
-            disableLights = Config.Bind("General", "disableLights", true, "Toggle the lights from any lamp or light source");
+        private ConfigEntry<bool> NoPlayerSpeedlines;
+
+        public void Awake() {
+            Logger.LogInfo("MoreFps by Moruto loaded!");
+            SceneManager.sceneLoaded += SceneLoadEvent;
+
+            disableBloom = Config.Bind("Lighting", "DisableBloom", true, "Toggle the Bloom (glowing lights) from doorFrames, lamps, weapons, etc...");
+            disableLights = Config.Bind("Lighting", "DisableLights", true, "Toggle the lights from any lamp or light source");
+
+            CustomResolution = Config.Bind("Resolution", "Resolution", "1920x1080", "Change the resolution of the game");
+            isFullscreen = Config.Bind("Resolution", "isFullscreen", true, "Make the game window fullscreen or not");
+
+            NoPlayerSpeedlines = Config.Bind("Particles", "NoPlayerSpeedlines", false, "Toggle the player's speed lines");
         }
 
-        private void sceneLoadEvent(Scene scene, LoadSceneMode mode) {
-            if (GameState.Instance.GetGraphics() == true) return;
+        public void Update() {
+            string[] resolutionVals = CustomResolution.Value.Split('x');
+            if (Screen.currentResolution.width.ToString() == resolutionVals[0] && Screen.currentResolution.height.ToString() == resolutionVals[1]) return;
 
+            Screen.SetResolution(int.Parse(resolutionVals[0]), int.Parse(resolutionVals[1]), isFullscreen.Value);
+        }
+
+        private void SceneLoadEvent(Scene scene, LoadSceneMode mode) {
             foreach (GameObject obj in scene.GetRootGameObjects()) {
-                if (obj.name.ToLower() == "bloom") {
-                    if (disableBloom.Value) Destroy(obj);
-                }
 
-                if (disableLights.Value) {
-                    if (obj.GetComponentsInChildren<Light>() != null) foreach (Light light in obj.GetComponentsInChildren<Light>()) { Destroy(light); }
-                }
+                if (obj.name == "Camera") if (NoPlayerSpeedlines.Value) Destroy(obj.GetComponentInChildren<ParticleSystem>());
+
+                if (GameState.Instance.GetGraphics() == true) return;
+                if (obj.name.ToLower() == "bloom") if (disableBloom.Value) Destroy(obj);
+                if (disableLights.Value) if (obj.GetComponentsInChildren<Light>() != null) foreach (Light light in obj.GetComponentsInChildren<Light>()) { Destroy(light); }
             }
         }
     }
