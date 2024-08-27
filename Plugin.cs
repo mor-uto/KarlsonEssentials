9f59c575b61e427dd6625d6cfcd1256757e2b54c
Moruto <79018613+mor-uto@users.noreply.github.com>
2024-08-28 02:04:07 +0300

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,37 +1,41 @@
 using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using HarmonyLib;
 
-namespace MoreFps {
-    [BepInPlugin("me.moruto.karlson.morefps", "MoreFps", "1.0.1")]
-    internal class Plugin : BaseUnityPlugin {
-        public ConfigEntry<bool> disableBloom;
-        public ConfigEntry<bool> disableLights;
-        public ConfigEntry<string> CustomResolution;
-        public ConfigEntry<bool> isFullscreen;
-        public ConfigEntry<bool> NoPlayerSpeedlines;
+[BepInProcess("Karlson.exe")]
+[BepInPlugin("me.moruto.plugins.karlsonessentials", "KarlsonEssentials", "1.0")]
+public class Plugin : BaseUnityPlugin {
+    public static new ManualLogSource Logger;
 
-        private FpsBoost fpsBoost;
+    private FpsBoost fpsBoost;
 
-        public void Awake() {
-            Logger.LogInfo("MoreFps by Moruto loaded!");
-            InitConfig();
-            new Harmony("me.moruto.karlson").PatchAll();
-            fpsBoost = new FpsBoost(this);
-        }
+    //Config
+    public ConfigEntry<bool> disableBloom;
+    public ConfigEntry<bool> disableLights;
+    public ConfigEntry<string> CustomResolution;
+    public ConfigEntry<bool> isFullscreen;
+    public ConfigEntry<bool> NoPlayerSpeedlines;
 
-        public void Update() {
-            fpsBoost.Update();
-        }
+    private void Awake() {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin KarlsonMod has been loaded!");
+        new Harmony("me.moruto.karlsonessentials").PatchAll();
+        initConfig();
+        fpsBoost = new FpsBoost(this);
+    }
 
-        private void InitConfig() {
-            disableBloom = Config.Bind("Lighting", "DisableBloom", true, "Toggle the Bloom (glowing lights) from doorFrames, lamps, weapons, etc...");
-            disableLights = Config.Bind("Lighting", "DisableLights", true, "Toggle the lights from any lamp or light source");
+    public void Update() {
+        fpsBoost.Update();
+    }
 
-            CustomResolution = Config.Bind("Resolution", "Resolution", "1920x1080", "Change the resolution of the game");
-            isFullscreen = Config.Bind("Resolution", "isFullscreen", true, "Make the game window fullscreen or not");
-
-            NoPlayerSpeedlines = Config.Bind("Particles", "NoPlayerSpeedlines", false, "Toggle the player's speed lines");
-        }
+    private void initConfig() {
+        disableBloom = Config.Bind("Lighting", "DisableBloom", true, "Toggle the Bloom (glowing lights) from doorFrames, lamps, weapons, etc...");
+        disableLights = Config.Bind("Lighting", "DisableLights", true, "Toggle the lights from any lamp or light source");
+        
+        CustomResolution = Config.Bind("Resolution", "Resolution", "1920x1080", "Change the resolution of the game");
+        isFullscreen = Config.Bind("Resolution", "isFullscreen", true, "Make the game window fullscreen or not");
+        
+        NoPlayerSpeedlines = Config.Bind("Particles", "NoPlayerSpeedlines", false, "Toggle the player's speed lines");
     }
 }
