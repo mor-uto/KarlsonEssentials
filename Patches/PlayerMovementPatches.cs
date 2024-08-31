8296fd8a2d48993086965281596dc257be7720ba
Moruto <79018613+mor-uto@users.noreply.github.com>
2024-08-31 08:52:22 +0300

    Update PlayerMovementPatches.cs

--- Patches/PlayerMovementPatches.cs
+++ Patches/PlayerMovementPatches.cs
@@ -5,12 +5,11 @@ internal class PlayerMovementPatches {
     [HarmonyPrefix]
     [HarmonyPatch("Look")]
     public static bool Prefix(PlayerMovement __instance) {
-        return Game.Instance.playing && !Plugin.fixCameraPostWin.Value;
+        if (!Game.Instance.playing && Plugin.fixNotPlayingCamera.Value) return false;
+        return true;
     }
 
     [HarmonyPrefix]
     [HarmonyPatch("SpeedLines")]
-    public static bool RemoveSpeedLines(PlayerMovement __instance) {
-        return Plugin.NoPlayerSpeedlines.Value ? false : true;
-    }
+    public static bool RemoveSpeedLines(PlayerMovement __instance) => !Plugin.noPlayerSpeedlines.Value;
 }
