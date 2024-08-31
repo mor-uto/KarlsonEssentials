1f31197fa8cc99ffa65754bf6e58b571accc14a4
Moruto <79018613+mor-uto@users.noreply.github.com>
2024-08-31 08:51:53 +0300

    Update FpsBoost.cs

--- FpsBoost.cs
+++ FpsBoost.cs
@@ -7,16 +7,16 @@ internal class FpsBoost {
     }
 
     public void Update() {
-        string[] resolutionVals = Plugin.CustomResolution.Value.Split('x');
+        string[] resolutionVals = Plugin.customResolution.Value.Split('x');
         if (Screen.currentResolution.width.ToString() == resolutionVals[0] && Screen.currentResolution.height.ToString() == resolutionVals[1]) return;
         Screen.SetResolution(int.Parse(resolutionVals[0]), int.Parse(resolutionVals[1]), !Plugin.isWindowed.Value);
     }
 
      private void SceneLoadEvent(Scene scene, LoadSceneMode mode) {
         foreach (GameObject obj in scene.GetRootGameObjects()) {
-            if (obj.name == "Camera") if (Plugin.NoPlayerSpeedlines.Value) UnityEngine.Object.Destroy(obj.GetComponentInChildren<ParticleSystem>());
+            if (obj.name == "Camera") if (Plugin.noPlayerSpeedlines.Value) UnityEngine.Object.Destroy(obj.GetComponentInChildren<ParticleSystem>());
 
-            if (Plugin.NoJumpPadEffect.Value && obj.name == "World") {
+            if (Plugin.noJumpPadEffect.Value && obj.name == "World") {
                 foreach (Transform child in obj.transform) {
                     if (child.name.Contains("JumpPad")) {
                         foreach (Transform grandchild in child) {
@@ -32,9 +32,7 @@ internal class FpsBoost {
                 }
             }
 
-
-            if (GameState.Instance.GetGraphics() == true) return;
-
+            if (obj.GetComponent<ReflectionProbe>() != null && Plugin.disableReflections.Value) GameObject.Destroy(obj);
             if (obj.name.ToLower() == "bloom") if (Plugin.disableBloom.Value) UnityEngine.Object.Destroy(obj);
             if (Plugin.disableLights.Value) if (obj.GetComponentsInChildren<Light>() != null) foreach (Light light in obj.GetComponentsInChildren<Light>()) { UnityEngine.Object.Destroy(light); }
         }
